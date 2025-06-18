a8f0f45fd85db04f0af368e4df2fc94f21977a78
philippe_di_quinzio <philippe.di.quinzio@usherbrooke.ca>
2025-06-17 22:05:48 -0400

    add gitflow notes

---
+++ z_notes/gitflow_notes.py
@@ -0,0 +1,29 @@
+#✅ Typical Workflow Example
+"""Start a new feature:
+
+bash
+Copier
+Modifier
+git checkout develop
+git checkout -b feature/my-new-feature
+When done:
+
+bash
+Copier
+Modifier
+git checkout develop
+git merge feature/my-new-feature
+git branch -d feature/my-new-feature
+When ready to release:
+
+bash
+Copier
+Modifier
+git checkout -b release/1.0 develop
+# bump version, fix bugs...
+git checkout main
+git merge release/1.0
+git tag -a v1.0 -m "Release 1.0"
+git checkout develop
+git merge release/1.0
+git branch -d release/1.0"""
