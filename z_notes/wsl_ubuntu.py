0ab51968fe99d6bf939f250b94d84672a55f1b8e
philippe_di_quinzio <philippe.di.quinzio@usherbrooke.ca>
2025-06-23 16:24:40 -0400

    adding new notes scond try 2

--- z_notes/wsl_ubuntu.py
+++ z_notes/wsl_ubuntu.py
@@ -1,9 +1,26 @@
 """
-gh auth login
-gh --version
 
-Strart claude command :
-claude
+wsl --version
+
+to see my machines installed :
+
+wsl --list --verbose
+
+to start the installed machine:
+wsl -d Ubuntu
+
+exit ubuntu machine :
+exit
+
+On Ubuntu machine:
+
+    gh auth login
+    gh --version
+    before starting claude go to proper repo 
+    cd /mnt/c/Users/Philippe/coding/git/base_infrastructure
+
+    Strart claude command :
+    claude
 
 
 """
