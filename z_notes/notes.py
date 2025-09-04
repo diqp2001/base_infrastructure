58ea26d931e8646de8c002cd5abb64714a7cef56
philippe_di_quinzio <philippe.di.quinzio@usherbrooke.ca>
2025-09-04 17:24:53 -0400

    running adjustement

--- z_notes/notes.py
+++ z_notes/notes.py
@@ -14,4 +14,8 @@ py -m pip install optuna
 
 pip freeze > requirements.txt
 py -m unittest discover -s src/tests -p "test_*.py" -v
+.venv\Scripts\Activate.ps1
+deactivate
+pip list
+pip install -r requirements.txt
 """ 
