b1bf863121613cc5ec8e48537b589fa4d556ac7f
Philippe Di Quinzio <77473228+diqp2001@users.noreply.github.com>
2025-09-18 18:29:16 -0400

    Update to_do.py

--- z_notes/to_do.py
+++ z_notes/to_do.py
@@ -5,6 +5,12 @@ excel pull data example
 interactive broker example
 
 consultation in strategy dev 
+data managwr app training data generation
+
+create exe
+deploy dockey
+and fx bonds commo data
+
 
 
 
