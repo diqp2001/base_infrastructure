3d6b844bf1d8b5cc1142271e161531da24750208
philippe_di_quinzio <philippe.di.quinzio@usherbrooke.ca>
2026-01-21 23:05:15 -0500

    swap leg modif

--- src/infrastructure/models/finance/financial_assets/derivative/swap/swap_leg.py
+++ src/infrastructure/models/finance/financial_assets/derivative/swap/swap_leg.py
@@ -12,8 +12,6 @@ class SwapLegModel(DerivativeModel):
     __tablename__ = 'swap_legs'
 
     swap_id = Column(Integer, ForeignKey('swaps.id'), nullable=False)
-    currency_id = Column(Integer, ForeignKey('currencies.id'), nullable=True, index=True)
-    underlying_asset_id = Column(Integer, ForeignKey('financial_assets.id'), nullable=True, index=True)
     
     # Primary key is also foreign key to parent
     id = Column(Integer, ForeignKey("derivatives.id"), primary_key=True)
@@ -21,8 +19,6 @@ class SwapLegModel(DerivativeModel):
     
     # Relationships
     swaps = relationship("src.infrastructure.models.finance.financial_assets.derivative.swap.swap.SwapModel", back_populates="swap_legs")
-    currency = relationship("src.infrastructure.models.finance.financial_assets.currency.CurrencyModel", foreign_keys=[currency_id])
-    underlying_asset = relationship("src.infrastructure.models.finance.financial_assets.financial_asset.FinancialAssetModel", foreign_keys=[underlying_asset_id])
     
     __mapper_args__ = {
     "polymorphic_identity": "swap_leg",
